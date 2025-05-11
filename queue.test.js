c66b33a005aa6ccf6a776cb4b34bcaf4333c0381
MicheleFeitosa <michelefeitosa08@gmail.com>
2025-05-11 13:12:46 -0300

    final challenge solution

--- queue.test.js
+++ queue.test.js
@@ -8,18 +8,32 @@ describe('Queue', () => {
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add(10)
+    queue.add(20)
+    expect(queue.size()).toBe(2)
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue()
+    queue.add(5)
+    expect(queue.elements).toEqual([5])
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+    queue.add(5)
+    queue.add(4)
+    expect(queue.peek()).toBe(5)
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue();
+    queue.add(8)
+    queue.add(4)
+    queue.add(2)
+    const removed = queue.dequeue()
+    expect(removed).toBe(8)
+    expect(queue.elements).toEqual([4, 2]);
   })
 })
